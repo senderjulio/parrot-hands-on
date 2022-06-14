6f8bc793350054fc3e56fefb26f9064829fc8d89
ZaidaCueto <zcueto58@gmail.com>
2022-06-14 12:55:36 -0300

    corrigindo conflito

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -1,22 +1,16 @@
-// import { Container } from 'react-bootstrap';
 import FormComponent from '../../Components/FormComponent';
 import GroupInput from '../../Components/GroupInput';
-
 
 // import { Container } from './styles';
 
 const Login = () => {
 
   return (
-    <FormComponent titulo='Login'>
+    <FormComponent titulo='LOGIN'>
+      <GroupInput key={1} type='email' placeholder='escreva seu email'/>
+      <GroupInput key={2} type='password' placeholder='escreva sua senha'/>
+    </FormComponent>
+  )
+}
 
-      <FormComponent titulo='LOGIN'>
-        <GroupInput key={1} type='email' placeholder='escreva seu email' />
-        <GroupInput key={2} type='password' placeholder='escreva sua senha' />
-   </FormComponent>
-
-      )
-   
-
-
-      export default Login
+export default Login;
