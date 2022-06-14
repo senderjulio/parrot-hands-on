74d804b3272c8e3aa292a76f3af8b582dbeaf4bc
ZaidaCueto <zcueto58@gmail.com>
2022-06-13 22:53:31 -0300

    feat: creado pege feed

---
+++ front-end/src/pages/Feed/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../Components/Header';
+
+
+// import { Container } from './styles';
+
+const Feed: React.FC = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default Feed;
