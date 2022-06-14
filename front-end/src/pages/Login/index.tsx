9656e22e5e94253d24e42bab25a09471d798c1e4
ZaidaCueto <zcueto58@gmail.com>
2022-06-13 22:51:50 -0300

    feat: adicionador routas de feed e perfil do usuario

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -1,15 +1,19 @@
+import { Container } from 'react-bootstrap';
 import FormComponent from '../../Components/FormComponent';
 import GroupInput from '../../Components/GroupInput';
+
 
 // import { Container } from './styles';
 
 const Login = () => {
 
   return (
+  
     <FormComponent titulo='Login'>
       <GroupInput key={1} type='email' placeholder='escreva seu email'/>
       <GroupInput key={2} type='password' placeholder='escreva sua senha'/>
     </FormComponent>
+
   )
 }
 
--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from './pages/Cadastro';
 import Login from './pages/Login';
+import PerfilDoUsuario from './pages/PerfilDoUsuario';
+import Feed from './pages/Feed';
 
 // import { Container } from './styles';
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/cadastro" element={<Cadastro/>} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/perfil-do-usuario" element={<PerfilDoUsuario />} />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
     </BrowserRouter>
   )
 }
